e1a1b9739429982d96b808d4b63b358c15f2ef1a
Brigitte Smith <Bsmith179@student.cscc.edu>
2026-05-14 20:30:48 -0400

    Created main page PizzaBuilderScreen.kt to be set by MainActivity onCreate. The new screen has a place order button and 1 listed topping right now.

--- app/src/main/java/com/example/codapizza/MainActivity.kt
+++ app/src/main/java/com/example/codapizza/MainActivity.kt
@@ -8,11 +8,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ToppingCell(
-                topping = Topping.Pepperoni,
-                placement = ToppingPlacement.Left,
-                onClickTopping = {}
-            )
+            PizzaBuilderScreen()
         }
     }
 
